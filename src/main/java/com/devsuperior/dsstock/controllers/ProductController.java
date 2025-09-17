3436aacfc2cd89f7478aaf3b35ee4a87a755319f
Nelio Alves <acenelio@gmail.com>
2025-09-17 11:40:51 -0300

    Retrieve products with pagination

---
+++ src/main/java/com/devsuperior/dsstock/controllers/ProductController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dsstock.controllers;
+
+import com.devsuperior.dsstock.entities.Product;
+import com.devsuperior.dsstock.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public Page<Product> getProducts(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+}
---
+++ src/main/java/com/devsuperior/dsstock/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dsstock.repositories;
+
+import com.devsuperior.dsstock.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+}
