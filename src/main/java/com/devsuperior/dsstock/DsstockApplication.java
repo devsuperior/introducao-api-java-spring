d61f20117d5c5cbbea849d3fee33e7b98298ba59
Nelio Alves <acenelio@gmail.com>
2025-09-17 10:30:32 -0300

    First commit

---
+++ src/main/java/com/devsuperior/dsstock/DsstockApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsstockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/dsstock/DsstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
